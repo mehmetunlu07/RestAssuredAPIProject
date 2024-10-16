0a18634d2437df24c3b284aa42f87f35f1e97390
mehmetunlu07 <maxbusiness24@gmail.com>
2024-10-16 11:12:56 -0500

    Commit from Intellij

--- src/test/java/mytest/Basics.java
+++ src/test/java/mytest/Basics.java
@@ -36,7 +36,7 @@ public class Basics {
 //        System.out.println("Body :"+body);
 
         String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-                .body(new String(Files.readAllBytes(Paths.get("D:\\Interview\\DOCs\\APIs\\AddPlaceJson.json"))))
+                .body(new String(Files.readAllBytes(Paths.get("src/test/java/files/AddPlaceJson.json"))))
                 .when().post("maps/api/place/add/json")
                 .then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
                 .header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
